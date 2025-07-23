09b3cf8f7e026e3b68325a0bf1410c85d449754b
Telescope928 <928telescope@gmail.com>
2025-07-23 11:31:50 +0900

    javascript start

---
+++ js/01_js개요.js
@@ -0,0 +1,10 @@
+/*
+모든js파일명.js확장명
+*.js 파일 내부를 <script> 태그 내부라고 보면됨
+어떤 특정 js파일 명칭인지 알 수 없으므로 * 어떠한 이라는 표기를 작성한 것임
+
+*/
+
+function externalfn() {
+  alert("HTML 외부 파일에 작성된 EXTERNALfn() 함수 기능 호출되었습니다.");
+}
